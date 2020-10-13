a0dba3800c5af75c7fc20aa0ebd4771f39bb4275
The Barnacle <thomasparis56@gmail.com>
2020-10-13 04:16:10 +0200

    init new file

---
+++ bot.js
@@ -0,0 +1,93 @@
+// On prépare la configuration du bot
+
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const guild = new Discord.Guild();
+
+const setup_slots = require('./setup_slots.js');
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// On prépare l'import des commandes
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// On initialise les listes qui vont regrouper les joueurs inscrits
+
+let slots = setup_slots();
+console.log(slots.monday_a);
+console.log(typeof(slots.monday_a));
+
+module.exports = { slots };
+
+client.on('message', message => {
+
+	//On peut afficher l'id du channel dans lequel on tape la commande :
+	//console.log(`channel id : ${message.channel.id}`)
+
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args, slots);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(token);
